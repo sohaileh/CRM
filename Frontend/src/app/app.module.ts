41b4ffcbef407d2fe32b3730bc6438197734b849
majid <beighmaajid123@gmail.com>
2023-01-28 17:15:21 +0530

    purchase module updated made responsive

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,11 +1,10 @@
-import { DashboardModule } from './dashboard/dashboard.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { DashboardModule } from './dashboard/dashboard.module';
+import { PurchaseModule } from "./purchase/purchase.module";
 @NgModule({
   declarations: [
     AppComponent
@@ -14,7 +13,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    DashboardModule
+    DashboardModule,
+    PurchaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]

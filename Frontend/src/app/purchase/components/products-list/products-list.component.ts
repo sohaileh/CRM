c8b574896a680a6ca3de000e5e0144a0c1fad37c
IT <bhatsuhail9267@gmail.com>
2023-02-05 22:40:19 +0530

    fixed iamge styling in add-sales

--- Frontend/src/app/purchase/components/products-list/products-list.component.ts
+++ Frontend/src/app/purchase/components/products-list/products-list.component.ts
@@ -12,28 +12,7 @@ import { PurchaseService } from '../../services/purchase.service';
 
 export class ProductsListComponent implements OnInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
-<<<<<<< Updated upstream
-  xsDevice = false;
-  searchData = "";
-  purchaseData = [
-    {
-      _id: 1,
-      vehicle_no: "JK01A 1111",
-      fullName: "Suhail Bhat",
-      purchase_date: new Date(),
-      purchase_amount: 100,
-      balance_amount: 0
-    },
-    {
-      _id: 2,
-      vehicle_no: "JK01A 2222",
-      fullName: "Suhail Ahmad",
-      purchase_date: new Date(),
-      purchase_amount: 200,
-      balance_amount: 100
-    }
-  ];
-  constructor(private sharedService: SharedService,private purchaseService:PurchaseService) { }
+  constructor(public sharedService: SharedService,private purchaseService:PurchaseService) { }
   purchaseList!: MatTableDataSource<any>;
   displayedColumns = ["sell_id", "vehicle_no", "seller_name", "purchase_date", "totalAmount", "balanceAmount", "actions"]
   ngOnInit(): void {
@@ -46,19 +25,8 @@ export class ProductsListComponent implements OnInit {
         alert('Something went wrong')
       }
     })
-   // this.purchaseList = new MatTableDataSource(this.purchaseData);
-    this.sharedService.isSmallDevice().subscribe(result => {
-      this.xsDevice = result[0].mqAlias === "xs" ? true : false;
-    });
-=======
-
-  constructor(public sharedService: SharedService) { }
-  purchaseList!:MatTableDataSource<any>;
-  displayedColumns = ["sell_id", "vehicle_no", "fullName", "purchase_date", "purchase_amount", "balance_amount", "actions"]
-  ngOnInit(): void {
-    this.purchaseList = new MatTableDataSource(this.sharedService.purchaseData);
->>>>>>> Stashed changes
   }
+
   ngAfterViewInit() {
       this.purchaseList.paginator = this.paginator;
   }
--- backend/middleware/error.handler.js
+++ backend/middleware/error.handler.js
@@ -2,7 +2,7 @@ const CustomErrorHandler = require("../services/customErrorHandler");
 const {PRODUCTION}=require("../config");
 const errorHandler = (err, req, res, next) => {
     let statusCode = 500;
-    let message = PRODUCTION?"Internal server error":err.message;
+    let message = PRODUCTION==="true"?"Internal server error":err.message;
 
     if (err instanceof CustomErrorHandler) {
         return res.status(err.status).json({ message: err.message });
